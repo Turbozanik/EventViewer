f39d1661a42610663ade4392f4466c427dee58c4
roman.lapa <rpl-dev@barefootsolutions.com>
2018-11-19 10:05:16 +0300

    Field renamed

--- data/src/main/java/com/data/net/datasource/NetDataSource.kt
+++ data/src/main/java/com/data/net/datasource/NetDataSource.kt
@@ -7,6 +7,7 @@ import com.data.prefs.datasource.PrefsDataSource
 import io.reactivex.Flowable
 
 open class NetDataSource(prefsDataSource: PrefsDataSource, retrofitApiCreator: RetrofitApiCreator) {
+
     private val mRetrofitService = retrofitApiCreator.create(prefsDataSource)
 
     fun register(map: Map<String, String?>?): Flowable<BaseResponse<User?>?> {
--- presentation/src/main/java/com/view/ui/modules/auth/register/RegistrationFragment.kt
+++ presentation/src/main/java/com/view/ui/modules/auth/register/RegistrationFragment.kt
@@ -41,7 +41,7 @@ class RegistrationFragment : RegistrationFragmentContract.RegistrationFragment()
         }
     }
 
-    private lateinit var datePickerDialog: DatePickerDialog
+    private lateinit var mDatePickerDialog: DatePickerDialog
 
     override fun addCurrentSubComponent() {
         daggerController.addRegistrationFragmentSubComponent()
@@ -93,19 +93,19 @@ class RegistrationFragment : RegistrationFragmentContract.RegistrationFragment()
     private fun initEtBirthday() {
         val calendar = Calendar.getInstance()
 
-        datePickerDialog = DatePickerDialog(context,
-                                            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+        mDatePickerDialog = DatePickerDialog(context,
+                                             DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                                                 mEtBirthday.setText(
                                                         getString(R.string.view_date_format,
                                                                   dayOfMonth.toString(),
                                                                   (monthOfYear + 1).toString(),
                                                                   year.toString()))
-                                                datePickerDialog.dismiss()
+                                                 mDatePickerDialog.dismiss()
                                             }, calendar.get(Calendar.YEAR),
-                                            calendar.get(Calendar.MONTH),
-                                            calendar.get(Calendar.DAY_OF_MONTH))
+                                             calendar.get(Calendar.MONTH),
+                                             calendar.get(Calendar.DAY_OF_MONTH))
         mEtBirthday.setOnClickListener {
-            datePickerDialog.show()
+            mDatePickerDialog.show()
         }
     }
 
